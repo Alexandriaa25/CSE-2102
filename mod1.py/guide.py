2c7d27506490472e03a4c559f256a256e8579b83
Alexandria Freeman <alexandria.mae111@gmail.com>
2026-08-30 19:29:12 -0400

    first commit

---
+++ mod1.py/guide.py
@@ -0,0 +1,19 @@
+def hello(str):
+    word = str.lower()
+
+    print(f"Your word is: {word}")
+
+#to initalize the file to github, use:
+#git init
+#git add .
+#git commit -m "Initial commit"
+#git remote add origin <your-github-repo-url>
+#git push -u origin main
+
+
+
+
+#To stage files/folders to github, use the following commands
+#git add filename.ext
+#git commit -m "Add new file"
+#git push origin main
